fdf1a834c34db44128374b428252b7581f93c95f
github username <github email address>
2019-09-27 10:44:39 +0100

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+
+  root "products#index", as: "products"
+  post '/', to: "products#add", as: "add_product"
+
+
+  # root 'products#index'
+  # post '/' => 'products#add'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
